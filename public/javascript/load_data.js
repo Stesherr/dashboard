39c05e079f509c59fb7232a69fde2980347046c6
CrisIntriago <caintria@espol.edu.ec>
2023-11-06 10:00:15 -0500

    Datos dashboard

--- public/javascript/load_data.js
+++ public/javascript/load_data.js
@@ -1,119 +1,141 @@
-import {tiempoArr, precipitacionArr, uvArr, temperaturaArr} from './static_data.js';
+import {
+  tiempoArr,
+  precipitacionArr,
+  uvArr,
+  temperaturaArr,
+} from "./static_data.js";
 
-let fechaActual = () => new Date().toISOString().slice(0,10);
+let fechaActual = () => new Date().toISOString().slice(0, 10);
 
 let cargarPrecipitacion = () => {
-    //Obtenga la fecha actual
-    let actual = fechaActual();
+  //Obtenga la fecha actual
+  let actual = fechaActual();
 
-    //Defina un arreglo temporal vacío
-    let datos = []
+  //Defina un arreglo temporal vacío
+  let datos = [];
 
-    //Itere en el arreglo tiempoArr para filtrar los valores de precipitacionArr que sean igual con la fecha actual
-    for (let index = 0; index < tiempoArr.length; index++) {
-        const tiempo = tiempoArr[index];
-        const precipitacion = precipitacionArr[index]
-    
-        if(tiempo.includes(actual)) {
-          datos.push(precipitacion)
-        }
-      }
+  //Itere en el arreglo tiempoArr para filtrar los valores de precipitacionArr que sean igual con la fecha actual
+  for (let index = 0; index < tiempoArr.length; index++) {
+    const tiempo = tiempoArr[index];
+    const precipitacion = precipitacionArr[index];
 
-    //Con los valores filtrados, obtenga los valores máximo, promedio y mínimo
-    let max = Math.max(...datos)
-    let min = Math.min(...datos)
-    let sum = datos.reduce((a, b) => a + b, 0);
-    let prom = (sum / datos.length) || 0;
-
-    //Obtenga la referencia a los elementos HTML con id precipitacionMinValue, precipitacionPromValue y precipitacionMaxValue
-    let precipitacionMinValue = document.getElementById("precipitacionMinValue")
-    let precipitacionPromValue = document.getElementById("precipitacionPromValue")
-    let precipitacionMaxValue = document.getElementById("precipitacionMaxValue")
-
-    //Actualice los elementos HTML con los valores correspondientes
-    precipitacionMinValue.textContent = `Min ${min} [mm]`
-    precipitacionPromValue.textContent = `Prom ${ Math.round(prom * 100) / 100 } [mm]`
-    precipitacionMaxValue.textContent = `Max ${max} [mm]`
-
-}
-
-let cargarFechaActual = () => {
-  
-    //Obtenga la referencia al elemento h6
-    let coleccionHTML = document.getElementsByTagName("h6")
-
-    let tituloH6 = coleccionHTML[0]
-    //Actualice la referencia al elemento h6 con el valor de la función fechaActual()
-    tituloH6.textContent = fechaActual()
-
+    if (tiempo.includes(actual)) {
+      datos.push(precipitacion);
+    }
   }
 
+  //Con los valores filtrados, obtenga los valores máximo, promedio y mínimo
+  let max = Math.max(...datos);
+  let min = Math.min(...datos);
+  let sum = datos.reduce((a, b) => a + b, 0);
+  let prom = sum / datos.length || 0;
 
+  //Obtenga la referencia a los elementos HTML con id precipitacionMinValue, precipitacionPromValue y precipitacionMaxValue
+  let precipitacionMinValue = document.getElementById("precipitacionMinValue");
+  let precipitacionPromValue = document.getElementById(
+    "precipitacionPromValue"
+  );
+  let precipitacionMaxValue = document.getElementById("precipitacionMaxValue");
+
+  //Actualice los elementos HTML con los valores correspondientes
+  precipitacionMinValue.textContent = `Min ${min} [mm]`;
+  precipitacionPromValue.textContent = `Prom ${
+    Math.round(prom * 100) / 100
+  } [mm]`;
+  precipitacionMaxValue.textContent = `Max ${max} [mm]`;
+};
+
+let cargarFechaActual = () => {
+  //Obtenga la referencia al elemento h6
+  let coleccionHTML = document.getElementsByTagName("h6");
+
+  let tituloH6 = coleccionHTML[0];
+  //Actualice la referencia al elemento h6 con el valor de la función fechaActual()
+  tituloH6.textContent = fechaActual();
+};
 
 let cargarOpenMeteo = () => {
+  //URL que responde con la respuesta a cargar
+  let URL =
+    "https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&hourly=temperature_2m,precipitation_probability&daily=uv_index_max&timezone=auto";
 
-    //URL que responde con la respuesta a cargar
-    let URL = 'https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&hourly=temperature_2m,precipitation_probability&daily=uv_index_max&timezone=auto';
+  fetch(URL)
+    .then((responseText) => responseText.json())
+    .then((responseJSON) => {
+      //Respuesta en formato JSON
+      let plotRef = document.getElementById("plot1");
+      let plotRef2 = document.getElementById("plot2");
 
-    fetch(URL)
-        .then(responseText => responseText.json())
-        .then(responseJSON => {
+      //Etiquetas del gráfico
+      let labels = responseJSON.hourly.time;
 
-            //Respuesta en formato JSON
-            let plotRef = document.getElementById('plot1');
-            let plotRef2 = document.getElementById('plot2');
+      //Etiquetas de los datos
+      let data = responseJSON.hourly.temperature_2m;
+      let data2 = responseJSON.hourly.precipitation_probability;
+      let data3 = responseJSON.daily.uv_index_max;
 
-            //Etiquetas del gráfico
-            let labels = responseJSON.hourly.time;
+      //Objeto de configuración del gráfico
+      let config = {
+        type: "line",
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: "Temperature [2m]",
+              data: data,
+            },
+            {
+              label: "UV Index",
+              data: data3,
+            },
+          ],
+        },
+      };
+      let config2 = {
+        type: "line",
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: "Precipatation probability",
+              data: data2,
+            },
+          ],
+        },
+      };
 
-            //Etiquetas de los datos
-            let data = responseJSON.hourly.temperature_2m;
-            let data2 = responseJSON.hourly.precipitation_probability;
-            let data3 = responseJSON.daily.uv_index_max;
+      //Objeto con la instanciación del gráfico
+      let chart1 = new Chart(plotRef, config);
 
-            //Objeto de configuración del gráfico
-            let config = {
-                type: 'line',
-                data: {
-                    labels: labels,
-                    datasets: [
-                        {
-                            label: 'Temperature [2m]',
-                            data: data,
-                        },
-                        {
-                            label: 'UV Index',
-                            data: data3,
-                        }
-                    ]
-                }
-            };
-            let config2 = {
-                type: 'line',
-                data: {
-                    labels: labels,
-                    datasets: [
-                        {
-                            label : 'Precipatation probability',
-                            data: data2,
-                        }
-                    ]
-                }
-            }
+      let chart2 = new Chart(plotRef2, config2);
+    })
+    .catch(console.error);
+};
 
-            //Objeto con la instanciación del gráfico
-            let chart1 = new Chart(plotRef, config);
+cargarPrecipitacion();
+cargarFechaActual();
+cargarOpenMeteo();
 
-            let chart2 = new Chart(plotRef2, config2)
+let parseXML = (responseText) => {
+  // Parsing XML
+  const parser = new DOMParser();
+  const xml = parser.parseFromString(responseText, "application/xml");
+
+  console.log(xml);
+};
+
+//Callback
+let selectListener = (event) => {
+  let selectedCity = event.target.value;
+  console.log(selectedCity);
+};
+
+let loadForecastByCity = () => {
+  //Handling event
+  let selectElement = document.querySelector("select")
+  selectElement.addEventListener("change", selectListener)
+};
+
+loadForecastByCity();
 
 
-
-        })
-        .catch(console.error);
-
-}
-
-
-cargarPrecipitacion()
-cargarFechaActual()
-cargarOpenMeteo()
